7eba83413af411e77c54be05fedfd9e54f9efe5f
nikolay.vlaskin <nikolay.vlaskin@gmail.com>
2020-07-31 02:27:53 +0700

    add toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.Toast.makeText(this, "Toast", android.widget.Toast.LENGTH_SHORT).show();
     }
 }
